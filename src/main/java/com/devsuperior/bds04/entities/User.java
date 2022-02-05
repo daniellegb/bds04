439c49beca45cc3dd05afebb431dd75c84968bdf
Danielle Balogh <daniellegb@protonmail.com>
2022-02-05 18:14:03 -0300

    Oauth2

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -1,8 +1,12 @@
 package com.devsuperior.bds04.entities;
 
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -13,15 +17,22 @@ import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails, Serializable{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
@@ -31,10 +42,11 @@ public class User {
 		inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
-	public User() {		
+	public User(){		
 	}
 
 	public User(Long id, String email, String password) {
+		super();
 		this.id = id;
 		this.email = email;
 		this.password = password;
@@ -66,6 +78,45 @@ public class User {
 	
 	public Set<Role> getRoles() {
 		return roles;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
--- src/main/java/com/devsuperior/bds04/services/UserService.java
+++ src/main/java/com/devsuperior/bds04/services/UserService.java
@@ -2,17 +2,26 @@ package com.devsuperior.bds04.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.bds04.dto.UserDTO;
 import com.devsuperior.bds04.entities.User;
 import com.devsuperior.bds04.repositories.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private UserRepository repository;
@@ -29,6 +38,30 @@ public class UserService {
 			dtos.add(new UserDTO(entities.get(x)));
 		}
 		return dtos;
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findByEmail(String email) {
+		User obj = repository.findByEmail(email);
+		return new UserDTO(obj);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		} 
+		logger.info("User found: " + username);
+		return user;
 	}
 	
 }
